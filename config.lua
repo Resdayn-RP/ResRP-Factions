8e088dea17026b2c89756f6aca8a1b10ab155057
HebiKotei <c1yvan@outlook.com>
2023-04-07 22:49:36 +0100

    feat(tableConfig): basic table structure added

--- config.lua
+++ config.lua
@@ -3,4 +3,11 @@ local config = {}
 ---@class tableConfig
 local tableConfig = {}
 
+tableConfig['factions'] = {
+    name = nil,
+    factionId = nil,
+    members = {},
+    leader = {}
+}
+
 return config, tableConfig
