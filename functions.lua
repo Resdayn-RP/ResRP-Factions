4c0b03ea44b328352038f19ff0749c91e4f1cb9a
HebiKotei <c1yvan@outlook.com>
2023-04-07 22:54:49 +0100

    feat(functions): Basic get FactionID command

--- functions.lua
+++ functions.lua
@@ -1,4 +1,16 @@
 ---@class functions
 local functions = {}
 
+---@param name string
+---@return integer|nil dbid
+function functions.getGroupId(name)
+    local factionTable = HebiDB:getTable()
+    for _, Table in pairs(factionTable) do
+        for _, faction in pairs(Table) do
+            if faction.name == name then return faction.factionId end
+        end
+    end
+    return nil
+end     
+
 return functions
